ec026c4ebb2f6ff5ef146410413457c6f9cae8d8
Vyacheslav Mitin <vyacheslav.mitin@gmail.com>
2022-04-05 02:37:27 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# Модуль скачки с порнхаба, в зависимостях youtube-dl как отдельная программа в PATH
+# "youtube-dl --external-downloader aria2c -i --console-title"
+
+# Импорты
+import os
+import subprocess
+
+
