0c609e0b4d567b647daa18114ad0d46d7cee2658
VyacheslavMitin <57019622+VyacheslavMitin@users.noreply.github.com>
2023-10-05 00:09:42 +0400

    версионирование, комментарий к счетчику

--- main.py
+++ main.py
@@ -3,13 +3,14 @@
 from links import return_models
 from downloader import starting_download
 
+__version__ = 3.00
 
 def main():
     """Основная функция"""
     print("Загрузка роликов с PornHub".upper()),
     print(f"Количество моделей для загрузки: {len(return_models()):}\n")
     print("Список моделей для скачки:".upper())
-
+    # вывод списка моделей построчно с указанием номера в списке очередности
     count = 0
     for item in return_models():
         count += 1
