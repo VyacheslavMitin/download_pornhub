931531354612d025565f9ff25dc2086e000cc931
VyacheslavMitin <57019622+VyacheslavMitin@users.noreply.github.com>
2023-10-16 04:13:36 +0400

    быстрофикс списка моделей

--- dictionary_processing.py
+++ dictionary_processing.py
@@ -25,37 +25,7 @@ def return_data():
                           f"{PATH}{os.sep}{name_model}"})
         dict_priority_.update({name_model:
                               priority_model})
-        prioritized_model_shuffle_.append(item)
-
-    # # Создание списка - массива с моделями
-    # for i in dict_priority_.keys():
-    #     prioritized_model.append(i)
-    # # Создание второго списка для рандомизации значения моделей
-    # prioritized_model_shuffle_ = prioritized_model.copy()
-    # # TODO очень костыльно реализовал рандомизацию передаваемого списка, надо переделать
-    # random.shuffle(prioritized_model_shuffle_)
-    # for y in range(1000):  # добавление 1000 пустых элементов в список для будущей рандомизации
-    #     prioritized_model_shuffle_.append('')
-    #
-    # for key, value in dict_priority_.items():
-    #     if value == 3:  # по значению приоритета 3 удаляем объект из списка и добавляем в список в случайное место
-    #         prioritized_model_shuffle_.remove(key)
-    #         prioritized_model_shuffle_.insert(random.randint(100, 999), key)
-    #     elif value == 2:  # по аналогии с 3
-    #         prioritized_model_shuffle_.remove(key)
-    #         prioritized_model_shuffle_.insert(random.randint(50, 99), key)
-    #     elif value == 1:  # по аналогии с 3
-    #         prioritized_model_shuffle_.remove(key)
-    #         prioritized_model_shuffle_.insert(random.randint(0, 49), key)
-    #     else:  # если приходит значение вне приоритета от 1 до 3 - ошибка и выход
-    #         import sys
-    #         sys.exit('Ошибка, нет значения для приоритизации модели')
-    #
-    # while True:  # удаление пустых строк из списка, созданных ранее
-    #     if '' in prioritized_model_shuffle_:
-    #         prioritized_model_shuffle_.remove('')
-    #     else:
-    #         break
+        prioritized_model_shuffle_.append(name_model)
 
     return dict_link_, dict_path_, dict_priority_, prioritized_model_shuffle_
 
