98bee43ebb4646822cad45420ab3b0cdfb3bd851
VyacheslavMitin <57019622+VyacheslavMitin@users.noreply.github.com>
2023-10-23 12:55:35 +0400

    работа с выгрузкой базы данных

---
+++ git_push_database.py
@@ -0,0 +1,28 @@
+# Модуль для отправки в Гит (GitHub) базы данных, которая лежит не в проекте
+import os
+import shutil
+
+from configs import PLATFORM, DATABASE_MODELS, abs_path
+from telegram_notifications import tg_send_notifications
+
+
+def git_push_db():
+    """Функция для отправки базы данных в GIT"""
+    print("Отправка базы данных с моделями в GIT")
+    db = "models.db"
+
+    match PLATFORM:
+        case 'macbook':
+            print("Копирование базы в каталог программы на Macbook")
+            shutil.copy(DATABASE_MODELS, abs_path)
+            os.system(f"git add {db}")
+            os.system("git commit -m 'обновление БД в Git'")
+            os.system("git push")
+
+        case 'wifi_router':
+            import sys
+            sys.exit("Запущено не на той платформе! Выход с ошибкой.")
+
+
+if __name__ == '__main__':
+    git_push_db()
---
+++ start/push_database.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+cd '/Users/sonic/PycharmProjects/download_pornhub/'
+
+clear
+
+python3 git_push_database.py
